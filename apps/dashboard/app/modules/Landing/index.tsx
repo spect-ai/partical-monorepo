c0fa95ddcdb592cbae71a3e35d554f2d9c4b67c7
Aditya Veer <nakulparmar15@gmail.com>
2022-07-15 22:50:04 +0530

    dashboard stuff

--- apps/dashboard/app/modules/Landing/index.tsx
+++ apps/dashboard/app/modules/Landing/index.tsx
@@ -1,26 +1,79 @@
-import { Box, Button, Heading, Stack, Text } from 'degen';
+import { Box, Button, Heading, IconPlus, Stack, Text } from 'degen';
+import Link from 'next/link';
 import React from 'react';
-import { useMoralis } from 'react-moralis';
+import { useMoralis, useMoralisQuery } from 'react-moralis';
+import styled from 'styled-components';
+
+const ListContainer = styled(Box)`
+  cursor: pointer;
+  box-shadow: inset 0 0 0 0 rgb(255, 255, 255, 0.05);
+  &:hover {
+    box-shadow: inset 400px 0 0 0 rgb(255, 255, 255, 0.05);
+  }
+`;
 
 export default function Landing() {
-  const { isAuthenticated, isAuthenticating, authenticate } = useMoralis();
+  const { isAuthenticated, isAuthenticating, authenticate, user } =
+    useMoralis();
+  const { data, isLoading, error } = useMoralisQuery(
+    'EntityMapping',
+    (query) => query.equalTo('userAddress', user?.get('ethAddress')),
+    [user]
+  );
+
+  console.log({ data });
+
   return (
     <Box>
       {isAuthenticated ? (
         <Stack align="center">
-          <Heading>Choose your DAO</Heading>
-          <Text variant="label">Choose your DAO or create one</Text>
+          <Stack space="1">
+            <Heading>Your DAOs</Heading>
+            <Text variant="label">
+              Choose and import your DAO or create a new one
+            </Text>
+          </Stack>
+          <Box
+            backgroundColor="backgroundSecondary"
+            // borderRadius="2xLarge"
+            height="96"
+          >
+            {data.map((dao, index) => (
+              <Link key={dao.id} href={`/entity/${dao.get('entityAddress')}`}>
+                <ListContainer
+                  paddingY="4"
+                  paddingX="8"
+                  borderBottomWidth="0.375"
+                  transitionDuration="500"
+                >
+                  <Stack>
+                    <Text weight="semiBold" size="extraLarge">
+                      Dao {index}
+                    </Text>
+                    <Text variant="label">{dao.get('entityAddress')}</Text>
+                  </Stack>
+                </ListContainer>
+              </Link>
+            ))}
+          </Box>
+          <Button prefix={<IconPlus />} center variant="tertiary">
+            Create a new DAO
+          </Button>
         </Stack>
       ) : (
         <Stack align="center" space="8">
           <Stack space="1">
             <Heading>Welcome to Partical</Heading>
-            <Text variant="label">Connect your wallet to get started</Text>
+            <Text variant="label">
+              Good to see you here, Connect your wallet to get started
+            </Text>
           </Stack>
           <Button
             loading={isAuthenticating}
             onClick={() => {
-              authenticate();
+              authenticate({
+                signingMessage: 'Sign in to Partical',
+              });
             }}
           >
             Connect Wallet
---
+++ apps/dashboard/pages/entity/[address]/index.tsx
@@ -0,0 +1,12 @@
+import EntityDashboard from 'apps/dashboard/app/modules/entityDashboard';
+import Layout from '../../../app/common/layout';
+
+export function Address() {
+  return (
+    <Layout>
+      <EntityDashboard />
+    </Layout>
+  );
+}
+
+export default Address;
