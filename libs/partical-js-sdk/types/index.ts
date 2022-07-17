4894cfbe4db0d33fea2250570e1fa34651ff1850
Aditya Chakraborty <adityachakra16@gmail.com>
2022-07-17 01:15:03 -0500

    add types

--- libs/partical-js-sdk/types/index.ts
+++ libs/partical-js-sdk/types/index.ts
@@ -5,3 +5,22 @@ export type NamespaceMetadata = {
   key?: string;
   appId: string;
 };
+
+export type StreamIndex = {
+  objectId: string;
+  streamId: string;
+  entityAddress: string;
+  appId: string;
+  schemaName: string;
+  tags: string[];
+  schema: object; // TODO: type this
+};
+
+export type Entity = {
+  objectId: string;
+  entityAddress: string;
+  streamId: string;
+  url: string;
+  userAddress: string;
+  encryptedSymmetricKey: string;
+};
